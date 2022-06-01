a101403d30ecb8efa823c051d139eb4b31125d42
Sivanantham Padikkasu <pksivanantham@gmail.com>
2022-06-01 23:32:40 +0530

    db creation script added

---
+++ Scripts/DB_Create.sql
@@ -0,0 +1,12 @@
+-- Database: mydb
+
+-- DROP DATABASE IF EXISTS mydb;
+
+CREATE DATABASE mydb
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_India.1252'
+    LC_CTYPE = 'English_India.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
